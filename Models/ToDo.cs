f5e791ca396bff45210f0d65d5e8b73386838fd1
srisainavya4 <143917084+srisainavya4@users.noreply.github.com>
2024-06-20 16:25:49 -0700

    Final Commit

---
+++ Models/ToDo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter a description.")]
+        public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please Enter a due date.")]
+        public DateTime? DueDate { get; set; }
+        [Required(ErrorMessage ="Please Enter a Category.")]
+        public string CategoryId { get; set; } = string.Empty;
+        [ValidateNever]
+        public Category Category { get; set; } = null!;
+        [Required(ErrorMessage ="Please select a status")]
+        public string StatusId { get; set; } = String.Empty;
+        [ValidateNever]
+        public Status Status { get; set; } = null!;
+        public bool Overdue => StatusId == "open" && DueDate < DateTime.Today;
+    }
+}
